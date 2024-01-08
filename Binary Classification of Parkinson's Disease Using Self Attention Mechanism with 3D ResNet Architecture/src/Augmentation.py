e75f6ccff7889bfdcc1177036f58b840f411b389
nabilah-afrin <118561482+nabilah-afrin@users.noreply.github.com>
2024-01-08 22:10:34 +0600

    Create Augmentation.py

---
+++ Binary Classification of Parkinson's Disease Using Self Attention Mechanism with 3D ResNet Architecture/src/Augmentation.py
@@ -0,0 +1,58 @@
+import random
+
+from scipy import ndimage
+
+
+@tf.function
+def rotate(volume):
+    """Rotate the volume by a few degrees"""
+
+    def scipy_rotate(volume):
+        # define some rotation angles
+        angles = [-20, -10, -5, 5, 10, 20]
+        # pick angles at random
+        angle = random.choice(angles)
+        # rotate volume
+        volume = ndimage.rotate(volume, angle, reshape=False)
+        volume[volume < 0] = 0
+        volume[volume > 1] = 1
+        return volume
+
+    augmented_volume = tf.numpy_function(scipy_rotate, [volume], tf.float32)
+    return augmented_volume
+
+
+def train_preprocessing(volume, label):
+    """Process training data by rotating and adding a channel."""
+    # Rotate volume
+    volume = rotate(volume)
+    volume = tf.expand_dims(volume, axis=3)
+    return volume, label
+
+
+def validation_preprocessing(volume, label):
+    """Process validation data by only adding a channel."""
+    volume = tf.expand_dims(volume, axis=3)
+    return volume, label
+
+
+# Define data loaders.
+train_loader = tf.data.Dataset.from_tensor_slices((x_train, y_train))
+validation_loader = tf.data.Dataset.from_tensor_slices((x_test, y_test))
+
+batch_size = 2
+
+# this will be ultimately be fed into the model
+train_dataset = (
+    train_loader.shuffle(len(x_train))
+    .map(train_preprocessing)
+    .batch(batch_size)
+    .prefetch(2)
+)
+# Only rescale.
+validation_dataset = (
+    validation_loader.shuffle(len(x_test))
+    .map(validation_preprocessing)
+    .batch(batch_size)
+    .prefetch(2)
+)
